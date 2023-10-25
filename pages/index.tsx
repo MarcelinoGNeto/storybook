21fdc3c1faa2d5a608e9fd50a771b954f3098134
MarcelinoGNeto <marcelino.gneto@gmail.com>
2023-10-25 14:39:06 -0300

    Button storybook

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Button from "../src/Components/Button/Button";
 
 export default function HomeScreen() {
     return (
         <div>
             <h1>Home Page!</h1>
+            <Button>Botão simplão</Button>
         </div>
     )
 }
